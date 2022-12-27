7edb519673dae41f261fd336834d52754a3d4e74
Abhishek Dubey <7abhishek1410@gmail.com>
2022-12-27 13:17:38 +0530

    fixes

--- router/route.js
+++ router/route.js
@@ -13,33 +13,60 @@ router.get("/", (req, res)=>{
     res.send("Hello from the server")
 })
 
-router.post("/register", (req, res)=>{
+router.post("/register", async (req, res)=>{
     // res.json({message: req.body})
     const {name, email, phone, passion, password, cnfPassword} = req.body;
-    console.log(email)
+    
     if(!name || !email || !phone || !passion || !password || !cnfPassword)
     {
         res.status(422).json({error: "Please fill the data properly"})
     }
-    // res.send(req.body)
+    
 
-    User.findOne({email:email}).then((userExists)=>{
-        if(userExists)
+    // User.findOne({email:email}).then((userExists)=>{
+    //     if(userExists)
+    //     {
+    //         res.status(422).json({error: "User already exists"})
+    //     }
+    //     else
+    //     {
+    //         const user = new User({name, email, phone, passion, password, cnfPassword})
+
+    //     user.save().then(()=>{
+    //         res.status(201).json({message: "user registered successfully"})
+    //     }).catch((e)=>{
+    //         res.status(500).json({error: "server error, failed to register"})
+    //     })
+    //     }
+    // }).catch((e)=>{
+    //     console.log(e)
+    // })
+
+
+   try
+   {
+    const userExists = await User.findOne({email: email})
+
+    if(userExists)
+    {
+        res.status(422).json({error: "user already exists"})
+    }
+    else
+    {
+        const newUser = new User({name, email, phone, passion, password, cnfPassword})
+
+        try
         {
-            res.status(422).json({error: "User already exists"})
-        }
-
-        const user = new User({name, email, phone, passion, password, cnfPassword})
-
-        user.save().then(()=>{
-            res.status(201).json({message: "user registered successfully"}).catch((e)=>{
-                res.status(500).json({error: "server error, failed to registor"})
-            })
-        })
-
-    }).catch((e)=>{
+            const createUser = await newUser.save()
+            res.status(201).json({message:"user registered successfully"})
+            console.log(createUser)
+        }catch(e){
+            res.status(500).json({error: "sever error, user not registered"})
+        }   
+    }
+   }catch(e) {
         console.log(e)
-    })
+   }
 
     
 })
