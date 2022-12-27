ea8ae97b0e1c13de28b772d8b6fc778e5d27ef6e
Abhishek Dubey <7abhishek1410@gmail.com>
2022-12-27 12:46:52 +0530

    user-created

--- router/route.js
+++ router/route.js
@@ -1,6 +1,7 @@
 const express = require("express")
 const router = new express.Router()
 
+const User = require("../model/userSchema")
 
 
 // const middleware = (req, res, next) =>{
@@ -13,9 +14,34 @@ router.get("/", (req, res)=>{
 })
 
 router.post("/register", (req, res)=>{
-    console.log(req.body)
     // res.json({message: req.body})
-    res.send(req.body)
+    const {name, email, phone, passion, password, cnfPassword} = req.body;
+    console.log(email)
+    if(!name || !email || !phone || !passion || !password || !cnfPassword)
+    {
+        res.status(422).json({error: "Please fill the data properly"})
+    }
+    // res.send(req.body)
+
+    User.findOne({email:email}).then((userExists)=>{
+        if(userExists)
+        {
+            res.status(422).json({error: "User already exists"})
+        }
+
+        const user = new User({name, email, phone, passion, password, cnfPassword})
+
+        user.save().then(()=>{
+            res.status(201).json({message: "user registered successfully"}).catch((e)=>{
+                res.status(500).json({error: "server error, failed to registor"})
+            })
+        })
+
+    }).catch((e)=>{
+        console.log(e)
+    })
+
+    
 })
 
 
--- src/app.js
+++ src/app.js
@@ -5,7 +5,6 @@ dotenv.config({ path: "./config/config.env"})
 const app = express()
 const PORT = process.env.PORT 
 const router = require("../router/route")
-const User = require("../model/userSchema")
 
 app.use(express.json())
 app.use(router)
